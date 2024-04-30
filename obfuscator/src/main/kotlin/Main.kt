f161cfaf64f87c58eaf0d481621e0314e77f8a92
skpntsv <n.skopintsev@g.nsu.ru>
2024-04-30 17:04:20 +0700

    now we can obfuscate literals

--- obfuscator/src/main/kotlin/Main.kt
+++ obfuscator/src/main/kotlin/Main.kt
@@ -1,16 +1,38 @@
 package ru.nsu.skopintsev
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.File
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val directory = File("./src/test/kotlin")
+    val files = directory.walkTopDown().filter { it.isFile && it.extension == "java" }
+
+    files.forEach { file ->
+        println("Обфускация файла: ${file.nameWithoutExtension}: ${file.absolutePath}")
+        val originalContent = file.readText()
+        val obfuscatedContent = obfuscateJavaCode(originalContent)
+        file.writeText(obfuscatedContent)
+        println("Обфускация файла ${file.name} прошла успешно")
     }
+}
+
+fun obfuscateJavaCode(code: String): String {
+    val stringLiteralRegex = "\"(?:\\\\.|[^\\\\\"])*\"".toRegex()
+
+    var obfuscatedCode = code
+    stringLiteralRegex.findAll(code).forEach { matchResult ->
+        val originalString = matchResult.value
+        val encodedString = obfuscateString(originalString)
+        obfuscatedCode = obfuscatedCode.replace(originalString, encodedString)
+        println("\t$originalString -> $encodedString")
+    }
+
+    return obfuscatedCode
+}
+
+fun obfuscateString(input: String): String {
+    val builder = StringBuilder()
+    for (char in input) {
+        builder.append("\\u").append(String.format("%04x", char.code))
+    }
+    return builder.toString()
 }
